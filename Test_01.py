695b8a8b9b7caa0c70d3c3d22b22ba9223dfdb68
Makssonik <110926093+Makssonik@users.noreply.github.com>
2022-08-10 08:38:09 +0300

    Исправлена кавычка

--- Test_01.py
+++ Test_01.py
@@ -4,6 +4,6 @@ print('Жми ctrl+enter')
 a = input('Введите \'a\'')
 b = input('Введите \'b\'')
 if a >= b:
-    print(f'Выходит {a:^3.2f} больше чем {b:^3.2f})
+    print(f'Выходит {a:^3.2f} больше чем {b:^3.2f}')
 else:
     print('В рот наоборот')
