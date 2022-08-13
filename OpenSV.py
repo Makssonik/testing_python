d10d02c67918299af237109709ac1b04e5e2d2d0
Makssonik <110926093+Makssonik@users.noreply.github.com>
2022-08-13 23:04:38 +0300

    Добавлен файл с клмпьютерным зрением

---
+++ OpenSV.py
@@ -0,0 +1,43 @@
+## https://www.youtube.com/watch?v=1Yi3irfC8BA
+## Импорт OpenSV и назначение имени cv
+import cv2 as cv
+
+## вывод версии OpenSV
+print(cv.__version__)
+
+## Захват изоюражения с веб камеры, по умолчанию веб камера с id 0
+capture = cv.VideoCapture(0)
+
+# Подгружаем каскад с лицами
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+## Цикл выводит изображение с вебки пока не нажмешь ESC
+while True:
+    # Получаем изображение
+    ret, img = capture.read()
+
+    # Основной поиск лица. img - где искать.
+    # scaleFactor - размер.
+    # minNeighbors - сколько соседей может быть, точнее поиск будет.
+    # minSize - минимальный размер поискового квадрата
+    faces = face_cascade.detectMultiScale(img, scaleFactor=1.5, minNeighbors=5, minSize=(20, 20))
+
+    # Рисуем квадрат на изображении
+    for (x, y, w, h) in faces:
+        # img - где рисуем
+        # (x, y) - начало квадрата
+        # (x+w, y+h) - конец квадрата
+        # (0, 0, 255) - цвет
+        # 2 - толщина
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    cv.imshow('From camera', img) # Показывает изображение с заголовком
+    k = cv.waitKey(30) & 0xFF # Ждет нажатия кнопки ESC 30 милисек и все это в интегрер
+    if k == 27:
+        break
+
+# Прекращает использовать веб камеру
+capture.release()
+
+## Закрывает все окна
+cv.destroyAllWindows()
